5f5685a6b12781ee5dfac0f6d6c3460ededaff15
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-01-15 14:38:43 +0100

    added one edge case

--- protocol/src/markets.rs
+++ protocol/src/markets.rs
@@ -234,7 +234,8 @@ mod tests {
 		let market_no_order = contract.get_market_order(0, 0);
 		assert_eq!(market_no_order.is_none(), false);
 		
-		contract.place_order(0, 1, 10000, 50);
+		contract.place_order(0, 1, 9000, 50);
+		contract.place_order(0, 1, 1000, 50);
 
 		let market_no_order = contract.get_market_order(0, 0);
 		let market_yes_order = contract.get_market_order(0, 1);
